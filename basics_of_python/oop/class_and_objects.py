798b53799971b4e4b43bee28bb3a2af8775ae805
Habibullah-Hisam <habibullahhisam@gmail.com>
2025-11-05 09:54:22 +0600

    Try to add default parameter to constructor.

--- basics_of_python/oop/class_and_objects.py
+++ basics_of_python/oop/class_and_objects.py
@@ -6,16 +6,25 @@ class Student:
     print(f"Name: {self.name}\nId: {self.id}\nCGPA: {self.cgpa}")
 
 class Person():
+  
   # constructor
   def __init__(self,name,age,occupation):
     self.name = name
     self.age = age
     self.ocupation = occupation
+  '''def __init__(self):
+    self.name = "abc"
+    self.age = 0
+    self.ocupation = "SD"
+    '''
+  def info(self):
+    print(f"Name: {self.name} \nage: {self.age} \nOcupation: {self.ocupation}")
+  
 
 st1 = Student()
 print(st1.name)
 st1.info()
-preson1 = Person("Bro",15,"Student")
-print(preson1.name)
-print(preson1.age)
-print(preson1.ocupation)
+person1 = Person("Bro",15,"Student")
+person1.info()
+# person2 = Person()
+# print(person2.name, person2.age, person2.ocupation)
