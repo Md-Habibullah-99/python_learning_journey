f1f165a542fecb5a0ca4cb6f6baa1273cbf7e03b
Habibullah-Hisam <habibullahhisam@gmail.com>
2025-11-11 21:32:45 +0600

    Add some more code.

--- basics_of_python/exc/news_app/write_the_json_file.py
+++ basics_of_python/exc/news_app/write_the_json_file.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 def generate_news():
-  news_type = input("Enter the news type(eng: tesla, bitconi, samsung, politics etc): ")
+  news_type = input("Enter the news type(eng: tesla, bitcoin, samsung, politics etc): ")
 
   url = f"https://newsapi.org/v2/everything?q={news_type}&from=2025-10-09&sortBy=publishedAt&apiKey=377b656dd442486b918cfcdc88be39c7"
 
