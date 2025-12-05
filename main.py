89abe6c888cb69fa332e4585375b699eecb1e4b5
md-habibullah-99 <habibullahhisam@gmail.com>
2025-12-05 20:50:27 +0600

    Write 'hello world' in tkinter.

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="Hello world")
+# Shoving it onto the screen
+myLabel.pack()
+
+
+root.mainloop()
+
+
