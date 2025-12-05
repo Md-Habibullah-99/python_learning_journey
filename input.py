a95729a22d5857a520dbcf5ad3340d872aa0b868
md-habibullah-99 <habibullahhisam@gmail.com>
2025-12-06 02:00:25 +0600

    Learned about input methods.

---
+++ input.py
@@ -0,0 +1,15 @@
+
+from tkinter import *
+import os
+
+root = Tk()
+
+e = Entry(root, width=50, bg="gray", borderwidth=7)
+e.pack()
+myButton1 = Button(root, text="Click Me!", fg="white", bg="gray")
+myButton1.pack()
+
+
+root.mainloop()
+
+
