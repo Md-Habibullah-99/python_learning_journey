74eded7035908b934e26ec7684d3a9ced59f82f3
md-habibullah-99 <habibullahhisam@gmail.com>
2025-12-05 23:51:03 +0600

    Apply grid system.

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from tkinter import *
 root = Tk()
 
 # Creating a Label Widget
-myLabel = Label(root, text="Hello world")
+myLabel1 = Label(root, text="Hello world")
+myLabel2 = Label(root, text="I am Habibullah")
+myLabel3 = Label(root, text="Studying in CSE")
 # Shoving it onto the screen
-myLabel.pack()
+
+myLabel1.grid(row=0,column=2)
+myLabel2.grid(row=1,column=18)
+myLabel3.grid(row=5,column=10)
 
 
 root.mainloop()
