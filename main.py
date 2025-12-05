e472462db52be0629a073d36e873681e77bae7ed
md-habibullah-99 <habibullahhisam@gmail.com>
2025-12-05 23:54:10 +0600

    Apply oop things.

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ from tkinter import *
 root = Tk()
 
 # Creating a Label Widget
-myLabel1 = Label(root, text="Hello world")
-myLabel2 = Label(root, text="I am Habibullah")
-myLabel3 = Label(root, text="Studying in CSE")
-# Shoving it onto the screen
-
-myLabel1.grid(row=0,column=2)
-myLabel2.grid(row=1,column=18)
-myLabel3.grid(row=5,column=10)
+myLabel1 = Label(root, text="Hello world").grid(row=0,column=2)
+myLabel2 = Label(root, text="I am Habibullah").grid(row=1,column=18)
+myLabel3 = Label(root, text="Studying in CSE").grid(row=5,column=10)
 
 
 root.mainloop()
